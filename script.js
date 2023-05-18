bbd4ab9c76a8ae7ab3fb9b6f5abdd01101cc3d38
CCotuna <calin.cotuna@emanuel.ro>
2023-05-18 23:26:16 +0300

    Created buttons for adding 1 pipe/random number of pipes and for sorting the pipes

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+let addButton;
+let randomButton;
+let sortButton;
+let pipes = [];
+
 function setup(){
     createCanvas(500,500);
-    background('red');
+    background('purple');
+
+    addButton = createButton('Add');
+    addButton.position(20,20);
+    
+    randomButton = createButton('Random');
+    randomButton.position(20,50);
+    
+    sortButton = createButton('Sort Pipes');
+    sortButton.position(20,80);
+    addButton.mousePressed(addPipes);
 }
+
 
 function draw(){
     
@@ -18,6 +34,19 @@ class Pipe {
 
     display(){
         fill(this.color);
-        rect(x, y, width, height);
+        rect(this.x, this.y, this.width, this.height);
     }
+}
+
+function addPipes(){
+    let x = 50;
+    const pipe = new Pipe(x + (pipes.length * 20), 50, 10, 50, '#D4ADFC');
+    pipes.push(pipe);
+    pipe.display();   
+}
+
+function displayPipe(){
+for(let i = 0; i<pipes.length; i++){
+    pipes[i].display();
+}
 }
